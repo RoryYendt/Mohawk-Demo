945a0e17c575cb41d170bdf8d75857b2a9bfb1b8
RoryYendt <yendtr@mcmaster.ca>
2016-04-20 21:54:06 -0400

    Minor Update
    Changed date checking, minor css changes. Added comment. Changed search
    to a type number.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 
 var db;
 
-MongoClient.connect('mongodb://rory:mohawk@ds013951.mlab.com:13951/mohawk-demo', (err, database) => {
+MongoClient.connect('mongodb://rory:mohawk@ds013951.mlab.com:13951/mohawk-demo', (err, database) => {//Connects to mongodb database hosted on mongolabs
   if (err) return console.log(err)
   db = database
   app.listen(3000, () => {
@@ -15,17 +15,17 @@ MongoClient.connect('mongodb://rory:mohawk@ds013951.mlab.com:13951/mohawk-demo',
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {//Sends the main page to the client
 	res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/app/patients', (req, res) => {
+app.get('/app/patients', (req, res) => {//Retrieve all patients
 	var cursor = db.collection('quotes').find().toArray(function(err,results){
 		res.send(results);
 	});
 })
 
-app.get('/app/patient', (req, res) => {
+app.get('/app/patient', (req, res) => {//Retrieve single patient
 	var query = {cardNumber: req.query.cardNumber};
 	db.collection('quotes', function(err, collection){
 		collection.findOne(query, function(err, item){
@@ -35,13 +35,12 @@ app.get('/app/patient', (req, res) => {
 })
 
 
-app.post('/app/patient', (req, res) => {
+app.post('/app/patient', (req, res) => {//Submit single patient
 	req.body._id = req.body.cardNumber;
-	console.log(req.body);
 	db.collection('quotes').save(req.body, (err, result) => {
 		if(err) return console.log(err);
 		console.log('Saved to database');
 		res.redirect('/');
 	})
 })
-console.log('May Node be with you');
+console.log('Running');
